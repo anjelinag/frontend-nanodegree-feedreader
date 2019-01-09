efced034d1a8e998fe2c3774cde41988cb293ddc
Anjelina Gulla <anjelina.sertse@gmail.com>
2019-01-08 20:40:41 -0500

    A Jasmine test that checks if all feeds have URLs defined and the URLs are not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('Has all URLs defined', function(){
+            allFeeds.forEach(function(aFeed){
+                url = aFeed.url;
+                expect(url).toBeDefined();
+                expect(url.length).not.tobe(0);
+            })
+        })
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
